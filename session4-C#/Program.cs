1736aec51cac7e9d61e1fe3f221d32928ee97c0d
m0azalaa <moazalaa434@gmail.com>
2025-07-26 06:41:47 +0300

    Q1

--- session4-C#/Program.cs
+++ session4-C#/Program.cs
@@ -2,24 +2,37 @@
 {
     internal class Program
     {
+        
+        #region Q1
+        // Passing By Value Type
+        static void swap(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        //Passing By Value Type
+        static void SwapByRef(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        #endregion
         static void Main(string[] args)
         {
-            //C44-G02-CS04
             #region Q1
-            // Passing By Value Type
-            static void swap(int x, int y)
-            {
-                int temp = x;
-                x = y;
-                y = temp;
-            }
-            //Passing By Value Type
-            static void SwapByRef(ref int x, ref int y)
-            {
-                int temp = x;
-                x = y;
-                y = temp;
-            }
+            //1.Passing By Value Type : is passing variable form ValueType is like sending a copy of our variables in the function
+            // that's why the main value of a,b doesn't chang
+            //Passing By Value :
+            //nothing will change in values
+            int a = 4, b = 5;
+            Console.WriteLine($"A : {a}"); //4
+            Console.WriteLine($"B : {b}"); //5
+            Console.WriteLine("----After Swap----");
+            swap(a, b);
+            Console.WriteLine($"A : {a}"); //4
+            Console.WriteLine($"B : {b}"); //5
             #endregion
         }
     }
