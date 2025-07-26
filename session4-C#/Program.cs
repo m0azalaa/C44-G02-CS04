d8a9d66410779ef6b621899228c551c62787e6e9
m0azalaa <moazalaa434@gmail.com>
2025-07-26 06:39:47 +0300

    Add project files.

---
+++ session4-C#/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace session4_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C44-G02-CS04
+            #region Q1
+            // Passing By Value Type
+            static void swap(int x, int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            //Passing By Value Type
+            static void SwapByRef(ref int x, ref int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            #endregion
+        }
+    }
+}
